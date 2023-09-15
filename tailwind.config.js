144068f7f795afc19d2be86df8db0c36158821db
Zach <itsaboy3366@gmail.com>
2023-09-15 19:08:53 -0400

    initial project setup, html header and footer complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./public/**/*.{html, js}", "./public/assets/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "Titillium"
+      }
+    },
+  },
+  plugins: [],
+}
+
